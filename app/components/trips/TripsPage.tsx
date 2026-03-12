7e9cc2caf12ba7486fa5d3b9fcf01438cb67e7d2
temu-cloud <temesgenmolla1994@gmail.com>
2026-03-12 19:56:05 +0300

    Fix trips page

--- app/components/trips/TripsPage.tsx
+++ app/components/trips/TripsPage.tsx
@@ -1,45 +1,50 @@
- 
 import EmptyListings from "../ui/EmptyListings";
 import ListingCard from "../listings/ListingCard";
 import { getCurrentUser } from "@/app/server-actions/getCurrentUser";
 import { getTrips } from "@/app/server-actions/getTrips";
 
-
 export default async function TripsPage() {
-    const trips = await getTrips();
-    const currentUser = await getCurrentUser();
+  const trips = await getTrips();
+  const currentUser = await getCurrentUser();
 
-    if (trips.length === 0) {
-           return (
-             <EmptyListings
-              title="No trips found"
-              subtitle="Looks like you haven’t booked any trips yet."
-            />
-           )
-          }
+  if (trips.length === 0) {
+    return (
+      <EmptyListings
+        title="No trips found"
+        subtitle="Looks like you haven’t booked any trips yet."
+      />
+    );
+  }
+
   return (
-       <div className="max-w-7xl mx-auto p-4">
-            <h2 className="text-2xl font-semibold mb-6">Your Trips</h2>
-      
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {trips.map((trip) => {
-                return (
-                  <ListingCard
-                    listing={trip.listing}                  
-                    key={trip.id}
-                    currentUser={currentUser}  
-                    reservation={{
-                        id:trip.id,
-                        startDate:trip.startDate,
-                        endDate:trip.endDate,
-                        totalPrice:trip.totalPrice
-                    }}     
-                    trip
-                    actionLabel="Cancel Trip"            
-                  />
-                );
-              })}
-            </div>
-          </div>
-    )
+    <div className="max-w-7xl mx-auto p-4">
+      <h2 className="text-2xl font-semibold mb-6">Your Trips</h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {trips.map((trip) => {
+          const safeListing = {
+            ...trip.listing,
+            createdAt: new Date(trip.listing.createdAt),
+            updatedAt: new Date(trip.listing.updatedAt),
+          };
+
+          return (
+            <ListingCard
+              listing={safeListing}
+              key={trip.id}
+              currentUser={currentUser}
+              reservation={{
+                id: trip.id,
+                startDate: trip.startDate,
+                endDate: trip.endDate,
+                totalPrice: trip.totalPrice,
+              }}
+              trip
+              actionLabel="Cancel Trip"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
